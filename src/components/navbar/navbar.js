fd318c49b57d6f42a173c134cd1f19d7421095d5
jamborpal <jamborpal0@gmail.com>
2023-05-29 22:04:02 +0200

    about is the first they see

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from './NavbarElements';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate('/about');
+  }, []);
+
   return (
     <>
       <Nav>
         <Bars />
 
         <NavMenu>
-          <NavLink exact to="/about" activeStyle>
+          <NavLink to="/about" activeStyle>
             About
           </NavLink>
           <NavLink to="/workexperience" activeStyle>
